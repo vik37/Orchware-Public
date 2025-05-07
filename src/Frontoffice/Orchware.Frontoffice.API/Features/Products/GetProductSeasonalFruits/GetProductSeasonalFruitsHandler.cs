c6427f939bf260b6f15bef3d6cd6685eae85a9a7
vik37 <vik.zafirovski@gmail.com>
2025-05-07 12:41:14 +0200

    feat: [Provide SeasonalFruits & Measures Enums via API for Frontend Select List] - API Get Product Seasonal/Measures Fruits

---
+++ src/Frontoffice/Orchware.Frontoffice.API/Features/Products/GetProductSeasonalFruits/GetProductSeasonalFruitsHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Orchware.Frontoffice.API.Features.Products.Shared.Enums;
+using Utilities.Converters;
+
+namespace Orchware.Frontoffice.API.Features.Products.GetProductSeasonalFruits;
+
+public class GetProductSeasonalFruitsHandler : IRequestHandler<GetProductSeasonalFruitsQuery,object>
+{
+	public async Task<object> Handle(GetProductSeasonalFruitsQuery request, CancellationToken cancellationToken)
+	{
+		var seasionalFruitsTask = Task.Run(() => Enum.GetValues(typeof(SeasonalFruits))
+										.Cast<SeasonalFruits>()
+										.Select(x => new
+										{
+											Index = (int)x,
+											Name = x.GetDisplayNameToString(),
+										}).ToList());
+
+		return await seasionalFruitsTask;
+	}
+}
---
+++ src/Frontoffice/Orchware.Frontoffice.API/Features/Products/GetProductUnitOfMeasures/ProductUnitOfMeasuresDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Orchware.Frontoffice.API.Features.Products.GetProductUnitOfMeasures;
+
+public class ProductUnitOfMeasuresDto
+{
+	public int Index { get; set; }
+	public string Name { get; set; } = string.Empty;
+	public string Fullname { get; set; } = string.Empty;
+}
